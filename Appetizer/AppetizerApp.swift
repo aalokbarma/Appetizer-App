e295745da41b9765da934a9d359a8fdb0c162a2d
Aalok Barma <Aalokbarma17@gmail.com>
2023-12-25 17:54:47 +0530

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Aalok Barma on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
